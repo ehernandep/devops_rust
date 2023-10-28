952f786c8015dde9aad4b512d3db16351a7ef55d
efhernandez-neostella <ehernandep@eafit.edu.co>
2023-10-27 20:22:11 -0500

    some changes

--- src/game_tests.rs
+++ src/game_tests.rs
@@ -1,38 +1,30 @@
-use actix_web::test;
-use actix_web::{App, test::TestRequest};
-use serde_json::Value;
-use crate::routes::game::get_games;
+use actix_web::{App, test};
+use crate::routes::game::get_games; // Replace with the actual import for your route handler.
 
 #[actix_rt::test]
 async fn test_get_games() {
-    // Create an App instance for testing.
-    let mut app = test::init_service(App::new().service(get_games)).await;
+    // Create a TestServer with the App.
+    let app = App::new().service(get_games); // Add your route handler to the App.
+
+    let mut app = test::init_service(app).await;
 
     // Simulate a GET request to /games.
-    let req = test::TestRequest::get().uri("/games").to_request();
+    let req = test::TestRequest::get().uri("/api/games").to_request();
     let resp = test::call_service(&mut app, req).await;
 
     // Check the response status.
     assert!(resp.status().is_success());
 
-    // Parse the response body as JSON.
-    let body: Value =  test::read_response_json(&mut app, req).await;
+    // Read the response body as bytes.
+    let response_bytes = test::read_body(resp).await;
 
+    // Now you can work with the response body as needed.
+    // For example, if you expect a JSON response:
+    let response_str = String::from_utf8_lossy(&response_bytes);
+    let response_json: serde_json::Value =
+        serde_json::from_str(&response_str).expect("Failed to parse JSON response");
 
-    // Verify the response JSON structure.
-    assert_eq!(body["status"], "success");
-    assert_eq!(body["no. games"], 2); // Adjust this based on your expected test data.
-
-    // Verify the content of the "games" array.
-    let games = body["games"].as_array().expect("Expected 'games' to be an array");
-    assert_eq!(games.len(), 2); // Adjust this based on your expected test data.
-
-    // Verify the properties of individual games.
-    let first_game = &games[0];
-    assert_eq!(first_game["address"], "calle 1");
-    // Add similar assertions for other properties of the first game.
-
-    let second_game = &games[1];
-    assert_eq!(second_game["address"], "calle 2");
-    // Add similar assertions for other properties of the second game.
+    // Now you can assert and work with the JSON response as needed.
+    assert_eq!(response_json["some_key"], "some_value"); // Replace with your expected JSON structure.
 }
+
